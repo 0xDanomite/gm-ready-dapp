eb3e4b9ddedce0bc9df7ecbe21a78d163ef9deeb
Dan McQuillan <daniel.mcquillan@protocol.ai>
2024-03-17 02:23:04 +0000

    removing

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
             </div>
           </DynamicWagmiConnector>
         </DynamicContextProvider>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
       </body>
     </html>
   );
